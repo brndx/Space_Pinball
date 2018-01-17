cabce9e39b360d9971f9d7e8cb88556ebde9c3b6
BlazinX <scary_dude_6@hotmail.com>
2018-01-17 15:54:50 +1100

    PleaseRevertAllTheseChanges
    Useless Changes.

---
+++ Assets/Source/PleaseDelete.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleaseDelete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //I don't know the unity functions so this will do.
+        Console.WriteLine("Hello Game!");
+        Console.ReadLine();
+        
+	}
+}
